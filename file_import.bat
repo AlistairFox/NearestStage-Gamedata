480defe08c42a0e7cd4f9842ff21112e1227cb1b
LuckyGuy <87475708+lInteller@users.noreply.github.com>
2023-07-25 15:03:47 +0400

    опять триипять

---
+++ file_import.bat
@@ -0,0 +1,4 @@
+taskkill /im xrEngine.exe /F
+move B:\Development\Projects\NearestStage\NearestStage-Gamedata\bin\*.dll B:\Development\Projects\NearestStage\OldEngine\
+cd C:\
+copy B:\Development\Projects\NearestStage\NearestStage-Engine\NearestStage\binares\x64-Release\*.dll B:\Development\Projects\NearestStage\NearestStage-Gamedata\bin\
